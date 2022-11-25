c8a7d4d452d85b0fd5390127ae89b1be9eb40ac3
Rachel Xie <rxie2002@gmail.com>
2022-11-24 23:47:44 -0500

    Cleaned up searchTesting file

--- LexiconSite/searchTesting.py
+++ LexiconSite/searchTesting.py
@@ -5,36 +5,32 @@ from pyairtable import Table
 from pyairtable.formulas import match
 
 # TODO: make the documentation reflect discoveries made here
-# NOTE: gives everything in the form of a nested dictionary
-
 # NOTE: my local code had a personal access token as a global variable here, you will have to generate your own
 #   following the instructions on Notion
 
 # TODO: function for getting the search term?
 
+# TODO: multiple types of search? (searching just name, searching through tags and text body and name? etc?)
+
+# TODO: a separate function to build the table? (see if statement below)
+
 
 def search_result(search_term: str, table: Table) -> str:
     """write a function that will take a search term, query the airtable API,
-    return a list of all the items with the"""
-    # note: how are you then going to display the results??
-    # going to need an OAuth access token -- i think for the time being we only need the data.records:read scope
-    # try using the OAuth token just in place of the API key?
-    # assume somewhere that you've grabbed the airtable API key, need a separate function to build the table
-    # here, build formula based on search term
-    # then, return list of results for displaying to front end??
-    # note that you then also will have to include pyairtable in the requirements.txt
-    # this will be a VERY rudimentary search (will only search the name of the term, looking for exact match, but:
-    # i don't know how the search will work with the tags
-    formula = match({"Name": search_term}, match_any=True)  # title will be whatever the name of the card is -- check air table for name -- could do with multiple columns?
+    return a list of all the items with the same term"""
+    # build formula based on search term
+    # this will be a VERY rudimentary search (will only search the name of the term, looking for exact match)
+    formula = match({"Name": search_term}, match_any=True)
 
-    # questions for everyone else: how are we doing the search bar?
-
+    # list of records that have the desired name
     results = table.all(formula=formula)
 
     return results
 
 if __name__ == '__main__':
-    table = Table(TEST_KEY, "appfaeFztiHKrh9DG", "Imported table")  # TEST_KEY is the removed personal access token
+    # building table: TEST_KEY is the removed personal access token, the second arg is base idea for test base,
+    #   third arg is the name of the table we want to look at
+    table = Table(TEST_KEY, "appfaeFztiHKrh9DG", "Imported table")
     result = search_result("心理學", table)
     print(result)
     if "心理學" in result[0]['fields']['Description']:
