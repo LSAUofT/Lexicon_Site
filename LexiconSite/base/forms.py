d257b052540e37b2b1224c7ea17ae6d5688d2e47
Rachel Xie <rxie2002@gmail.com>
2023-01-03 16:54:48 -0500

    Created ContactForm class

---
+++ LexiconSite/base/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+# note: going to Contact page causes "OSError: Bad URL"
+class ContactForm(forms.Form):
+    user_name = forms.CharField(label='Your name', max_length=100)
+    user_address = forms.EmailField(label='Your email', required=True)
+    user_message = forms.CharField(label='Your message')
