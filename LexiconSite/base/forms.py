df54ce67c208a9ccd79a3b3d93fa250f49a02426
Rachel Xie <rxie2002@gmail.com>
2023-01-17 15:19:35 -0500

    Contact form complete

--- LexiconSite/base/forms.py
+++ LexiconSite/base/forms.py
@@ -2,15 +2,24 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    # todo: modify widgets such that they have all the attributes from original .html file
-    user_name = forms.CharField(label='Your name', max_length=100,
+    user_name = forms.CharField(label='Name', max_length=100,
                                 widget=forms.TextInput(
-                                        attrs={'placeholder': 'Enter Your Name',
+                                        attrs={'placeholder': 'Enter your name',
                                                'class': 'u-border-1 u-border-grey-30 u-input u-input-rectangle u-white',
                                                'id': 'name-3b9a'}
-                                    )
-                                )
-    user_address = forms.EmailField(label='Your email', required=True)
-    user_message = forms.CharField(label='Your message', widget=forms.Textarea)
+                                ))
+    user_address = forms.EmailField(label='Email', required=True,
+                                    widget=forms.EmailInput(
+                                        attrs={'placeholder': 'Enter a valid email address',
+                                               'class': 'u-border-1 u-border-grey-30 u-input u-input-rectangle u-white',
+                                               'id': 'email-3b9a'
+                                               }
+                                    ))
+    user_message = forms.CharField(label='Message',
+                                   widget=forms.Textarea(
+                                       attrs={'placeholder': 'Enter your message',
+                                              'class': 'u-border-1 u-border-grey-30 u-input u-input-rectangle u-white',
+                                              'id': 'message-3b9a'}
+                                   ))
 
     # TODO: add option to cc yourself?
--- LexiconSite/base/views.py
+++ LexiconSite/base/views.py
@@ -35,28 +35,22 @@ def contact(request):
             # process the data in form.cleaned_data (a dict) as required
             print(form.cleaned_data)
 
-            # send_mail(
-            #     'Lex. Sci. Asia: Message Received',  # subject
-            #     form.cleaned_data['user_message'],  # email message
-            #     None,  # email that this message from -- b/c None, sent from default email (defined in settings)
-            #     ['lexscieasia@gmail.com'],  # email this is getting sent to (can have more than one)
-            #     fail_silently=False,
-            # )
+            send_mail(
+                'Lex. Sci. Asia: Message Received',  # subject
+                form.cleaned_data['user_message'],  # email message
+                None,  # email that this message from -- b/c None, sent from default email (defined in settings)
+                ['lexscieasia@gmail.com'],  # email this is getting sent to (can have more than one)
+                fail_silently=False,
+            )
 
-            # redirect to a new URL:
-            # this is going to try to send user to page with URL /thanks/ -- absolute path
-            # currently, WILL cause errors, something's up with the URLs
-            return HttpResponseRedirect('thanks/')  # this will take you to the associated URL
+            # redirect to a new URL (relative to current page):
+            return HttpResponseRedirect('thanks/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = ContactForm()
-        # # renders form according to the formatting described in nice_form.html
-        # rendered_form = form.render("base/nice_form.html")
-        # context = {'form': rendered_form}
         print("SECOND IF HERE")
 
-    # return render(request, "base/Contact.html", context)  # sends form information to be rendered
     return render(request, "base/Contact.html", {'form': form})
 
 
