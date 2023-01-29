b78b4de46853adf78155f14035e1207b5f1a51e6
Rachel Xie <rxie2002@gmail.com>
2023-01-28 21:45:52 -0500

    fixed views and urls so search pages display

--- LexiconSite/search/urls.py
+++ LexiconSite/search/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import SearchHomeView, SearchResultView
+from .views import SearchHomeView, SearchResultView, search, advanced_search
 
 urlpatterns = [
     # path('search/', SearchHomeView.as_view(), name='search_home'),  # initial search page
-    path('', SearchHomeView.as_view(), name='search_home'), # for testing, so it doesn't need to connect to other app yet
-    path('search/results/', SearchResultView.as_view(), name='search_results')  # search results page
+    # path('', SearchHomeView.as_view(), name='search_home'), # for testing, so it doesn't need to connect to other app yet
+    path('search/', search, name='search'),
+    # path('search/results/', SearchResultView.as_view(), name='search_results')  # search results page
+    path('search/advanced', advanced_search, name='advanced_search')
 ]
